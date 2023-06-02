54820b69437c5a5a9fc609bed8501b3406e71e3d
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-06-01 18:16:04 -0700

    completed schemas folder

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,70 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    me: async (parents, args, context) => {
+      if (context.user) {
+      data =await User.findOne({ _id: context.user._id })
+      .select('-__v -password')
+      return data;
+    }
+    throw new AuthenticationError('Not logged in.');
+  },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        throw new AuthenticationError('User not found. Do you have an account?');
+      }
+  
+      const correctPw = await user.isCorrectPassword(password);
+  
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials!');
+      }
+  
+      const token = signToken(user);
+  
+      return { token, user };
+    },
+    saveBook: async (parent, { newBook }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        context.user._id,
+        { $push: { savedBooks: newBook }},
+        { new: true }
+      );
+  
+      return updatedUser;
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (!context.user) {
+        throw new AuthenticationError('Login required!');
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        context.user._id,
+        { $pull: { savedBooks: { bookId }}},
+        { new: true }
+      );
+  
+      return updatedUser;
+    }
+  }
+  
+  
+};
+
+module.exports = resolvers;

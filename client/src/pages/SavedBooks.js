e15a28b74081a698c94e723da4ac91031c600d1a
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-06-01 22:06:52 -0700

    bug fix

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -15,7 +15,7 @@ import { GET_ME } from '../utils/queries';
 
 const SavedBooks = () => {
   const {loading, data} = useQuery(GET_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook,] = useMutation(REMOVE_BOOK);
 
   // use this to determine if `useEffect()` hook needs to run again
   const userData = data?.me || {};
